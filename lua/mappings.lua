7dd74f1fc8b631b83652cee4cea57d7bc044dd44
Kotatsu1 <nikitakotatsu@gmail.com>
2024-10-28 10:54:55 +0300

    c-u c-d fast scroll by 8 lines

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,7 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-
+map('n', '<C-d>', '8j', { noremap = true, silent = true })
+map('n', '<C-u>', '8k', { noremap = true, silent = true }
+)
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
