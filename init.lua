7d570e5aaf4b2b743f02e432011ca92dec7d35af
Kotatsu1 <nikitakotatsu@gmail.com>
2024-10-28 15:07:55 +0300

    better looking errors

--- init.lua
+++ init.lua
@@ -7,6 +7,37 @@ vim.opt.scrolloff = 5
 vim.opt.mouse = ''
 
 
+-- Diagnostic highlighting
+vim.api.nvim_create_autocmd({"CursorHold", "CursorMoved"}, {
+  callback = function()
+  vim.diagnostic.config({
+    virtual_text = false,
+    signs = true,
+    underline = true,
+    update_in_insert = false,
+  })
+    local float_opts = {
+      focusable = false,
+      close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+    }
+
+    if not vim.b.diagnostics_pos then
+      vim.b.diagnostics_pos = { nil, nil }
+    end
+
+    local cursor_pos = vim.api.nvim_win_get_cursor(0)
+    if
+      (cursor_pos[1] ~= vim.b.diagnostics_pos[1] or cursor_pos[2] ~= vim.b.diagnostics_pos[2])
+      and #vim.diagnostic.get() > 0
+    then
+      vim.diagnostic.open_float(nil, float_opts)
+    end
+
+    vim.b.diagnostics_pos = cursor_pos
+  end,
+})
+
+
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
